1f22e3b9f5962c2e5d6de298b06d3dea23a6842b
Simon Friedmann <simon.friedmann@kip.uni-heidelberg.de>
2015-04-22 17:12:07 +0200

    script update

--- synram.c
+++ synram.c
@@ -5,9 +5,10 @@
 #include "syn.h"
 
 static unsigned const NUM_SYN_LOCATIONS = 64 + 4;
+static unsigned const NUM_WRITES = 10;
 
 
-void synram_write_all(uint8_t data) {
+static void synram_write_all(uint8_t data) {
 #define VR_DATA 1
   int i;
   fxv_array_t wdata;
@@ -24,26 +25,52 @@ void synram_write_all(uint8_t data) {
 #undef VR_DATA
 }
 
-void synram_write_from_mailbox() {
+static void synram_write_from_mailbox() {
 #define VR_DATA 1
-  int i;
+#define VR_TMP 2
+  unsigned i, k;
   fxv_array_t wdata;
+
+  /*mailbox_read((uint8_t*)&wdata, 0, sizeof(wdata));*/
+  /*sync();*/
+  /*fxv_load_array(VR_DATA, &wdata);*/
 
   for(i=0; i<NUM_SYN_LOCATIONS; ++i) {
     mailbox_read((uint8_t*)&wdata, i * sizeof(wdata), sizeof(wdata));
     sync();
     fxv_load_array(VR_DATA, &wdata);
-    syn_store_decoders(VR_DATA, i);
+    syn_load_decoders(VR_TMP, i);
+
+    for(k=0; k<NUM_WRITES; ++k) {
+      syn_store_decoders(VR_DATA, i);
+    }
   }
+#undef VR_TMP
 #undef VR_DATA
 }
 
-void synram_read_all() {
+static void synram_write_once(unsigned loc, fxv_array_t* data) {
+#define VR_DATA 1
+#define VR_TMP 2
+  unsigned k;
+
+  fxv_load_array(VR_DATA, &data);
+  syn_load_decoders(VR_TMP, loc);
+
+  for(k=0; k<NUM_WRITES; ++k)
+    syn_store_decoders(VR_DATA, loc);
+
+#undef VR_TMP
+#undef VR_DATA
+}
+
+static void synram_read_all() {
 #define VR_DATA 1
   int i;
   fxv_array_t rdata;
 
   for(i=0; i<NUM_SYN_LOCATIONS; ++i) {
+    syn_load_decoders(VR_DATA, i);
     syn_load_decoders(VR_DATA, i);
 
     fxv_store_array(&rdata, VR_DATA);
@@ -56,7 +83,31 @@ void synram_read_all() {
 #undef VR_DATA
 }
 
-void synram_read() {
+
+static void synram_repeated_read() {
+#define VR_DATA 1
+  int i, j, k;
+  fxv_array_t rdata;
+
+  for(j=0; j<64; ++j) {
+    for(i=0; i<1; ++i) {
+      syn_load_decoders(VR_DATA, i);
+      syn_load_decoders(VR_DATA, i);
+
+      fxv_store_array(&rdata, VR_DATA);
+      sync();
+
+      mailbox_write((j*1 + i) * sizeof(rdata),
+          (uint8_t*)&rdata,
+          sizeof(rdata));
+    }
+
+    for(k=0; k<50*j; ++k) asm volatile("nop");
+  }
+#undef VR_DATA
+}
+
+static void synram_read() {
   fxv_array_t rdata;
 
   syn_load_decoders(1, 0);
@@ -65,19 +116,28 @@ void synram_read() {
   mailbox_write(0, (uint8_t*)&rdata, sizeof(rdata));
 }
 
-void synram_omni_write() {
-  uint32_t i;
+static void synram_omni_write() {
   volatile uint32_t* ptr = (volatile uint32_t*)(DECODER_BASE_ADDR);
 
   mailbox_read((volatile uint8_t*)ptr, 0, 33*16*4);
 }
 
 void start() {
+  unsigned k;
+  fxv_array_t wdata;
+
+  for(k=0; k<NUM_BYTES_PER_ARRAY; ++k)
+    wdata.bytes[k] = 0x3f;
+  sync();
+
   /*synram_write();*/
-  /*synram_write_all(0x01);*/
+  /*synram_write_all(0x3f);*/
   synram_write_from_mailbox();
+  synram_write_once(0, &wdata);
   /*synram_omni_write();*/
+  /*for(k=0; k<250; ++k) asm volatile("nop");*/
   synram_read_all();
+  /*synram_repeated_read();*/
 
   /*static uint32_t const tmp = 0xdeadface;*/
   /*mailbox_write(0, (uint8_t*)&tmp, sizeof(uint32_t));*/
